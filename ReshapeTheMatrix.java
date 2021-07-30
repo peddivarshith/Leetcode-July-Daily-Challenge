afaa9d1e6963c1317c861ce04d32f7db1de63ba6
peddivarshith <panduvarshith7562@gmail.com>
2021-07-30 11:26:06 +0530

    Day 5 program

---
+++ ReshapeTheMatrix.java
@@ -0,0 +1,51 @@
+/*
+In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
+
+You are given an m x n matrix mat and two integers r and c representing the row number and column number of the wanted reshaped matrix.
+
+The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[1,2],[3,4]], r = 1, c = 4
+Output: [[1,2,3,4]]
+Example 2:
+
+
+Input: mat = [[1,2],[3,4]], r = 2, c = 4
+Output: [[1,2],[3,4]]
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 100
+-1000 <= mat[i][j] <= 1000
+1 <= r, c <= 300
+*/
+
+class ReshapeTheMatrix {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int n=mat.length;
+        int m=mat[0].length;
+        if(n*m!=r*c)return mat;
+        int reshape[][]=new int[r][c];
+        int ele=0;
+        int ins_col=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int ins_row=ele/c;
+                reshape[ins_row][ins_col]=mat[i][j];
+                ins_col=(ins_col+1)%c;
+                ele++;
+            }
+        }
+        return reshape;
+    }
+}
